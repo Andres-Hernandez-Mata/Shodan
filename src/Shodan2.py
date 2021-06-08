030eb01bc6238c8d6abb75d598cde7a1ea66a9fa
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-06-07 20:14:59 -0500

    Buscar hosts

--- src/Shodan2.py
+++ src/Shodan2.py
@@ -13,7 +13,7 @@ api = getpass.getpass("Introduce tu API key: ")
 sites = []
 shodan = shodan.Shodan(api)
 resultados = shodan.search('port: 21 Anonymous user logged in')
-print("Número de hosts encontrados", len(resultados['matches']))
+print("Número de hosts encontrados ", len(resultados['matches']))
 for match in resultados['matches']:
     if match['ip_str'] is not None:
         print(match['ip_str'])
